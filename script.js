833a0a7f92e1d081c9248397441fb1e7df2eea4a
sunfeihao <71101136+sunfeihao@users.noreply.github.com>
2022-12-03 17:11:04 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,218 @@
+const data=[
+  {
+    "date": "2018-12-31",
+    "product": "Product A",
+    "investment": 14914,
+    "sales": 17151.1,
+    "revenue": 2237.1,
+    "revenueper": 15
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product A",
+    "investment": 8452,
+    "sales": 10226.92,
+    "revenue": 1774.92,
+    "revenueper": 21
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product A",
+    "investment": 6114,
+    "sales": 7275.66,
+    "revenue": 1161.66,
+    "revenueper": 19
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product B",
+    "investment": 14617,
+    "sales": 17686.57,
+    "revenue": 3069.57,
+    "revenueper": 21
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product B",
+    "investment": 15029,
+    "sales": 15479.87,
+    "revenue": 450.87,
+    "revenueper": 3
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product B",
+    "investment": 13131,
+    "sales": 15231.96,
+    "revenue": 2100.96,
+    "revenueper": 16
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product C",
+    "investment": 16614,
+    "sales": 19272.24,
+    "revenue": 2658.24,
+    "revenueper": 16
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product C",
+    "investment": 861,
+    "sales": 1076.25,
+    "revenue": 215.25,
+    "revenueper": 25
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product C",
+    "investment": 16818,
+    "sales": 17490.72,
+    "revenue": 672.72,
+    "revenueper": 4
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product D",
+    "investment": 12688,
+    "sales": 14210.56,
+    "revenue": 1522.56,
+    "revenueper": 12
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product D",
+    "investment": 6837,
+    "sales": 7725.81,
+    "revenue": 888.81,
+    "revenueper": 13
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product D",
+    "investment": 5520,
+    "sales": 5685.6,
+    "revenue": 165.6,
+    "revenueper": 3
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product E",
+    "investment": 1027,
+    "sales": 1252.94,
+    "revenue": 225.94,
+    "revenueper": 22
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product E",
+    "investment": 10512,
+    "sales": 10827.36,
+    "revenue": 315.36,
+    "revenueper": 3
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product E",
+    "investment": 2485,
+    "sales": 2957.15,
+    "revenue": 472.15,
+    "revenueper": 19
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product F",
+    "investment": 1990,
+    "sales": 2407.9,
+    "revenue": 417.9,
+    "revenueper": 21
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product F",
+    "investment": 8034,
+    "sales": 9399.78,
+    "revenue": 1365.78,
+    "revenueper": 17
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product F",
+    "investment": 13254,
+    "sales": 14314.32,
+    "revenue": 1060.32,
+    "revenueper": 8
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product G",
+    "investment": 18545,
+    "sales": 21141.3,
+    "revenue": 2596.3,
+    "revenueper": 14
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product G",
+    "investment": 6168,
+    "sales": 7524.96,
+    "revenue": 1356.96,
+    "revenueper": 22
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product G",
+    "investment": 12382,
+    "sales": 14610.76,
+    "revenue": 2228.76,
+    "revenueper": 18
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product H",
+    "investment": 12498,
+    "sales": 14622.66,
+    "revenue": 2124.66,
+    "revenueper": 17
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product H",
+    "investment": 8214,
+    "sales": 8953.26,
+    "revenue": 739.26,
+    "revenueper": 9
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product H",
+    "investment": 3361,
+    "sales": 3865.15,
+    "revenue": 504.15,
+    "revenueper": 15
+  },
+  {
+    "date": "2018-12-31",
+    "product": "Product I",
+    "investment": 16152,
+    "sales": 18413.28,
+    "revenue": 2261.28,
+    "revenueper": 14
+  },
+  {
+    "date": "2019-12-31",
+    "product": "Product I",
+    "investment": 2303,
+    "sales": 2349.06,
+    "revenue": 46.06,
+    "revenueper": 2
+  },
+  {
+    "date": "2020-12-31",
+    "product": "Product I",
+    "investment": 14806,
+    "sales": 18063.32,
+    "revenue": 3257.32,
+    "revenueper": 22
+  }
+];
