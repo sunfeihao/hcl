d9d6396d4ca892392ffc9617c5200c7664d54b76
sunfeihao <71101136+sunfeihao@users.noreply.github.com>
2022-12-03 19:24:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const data=[
   {
     "date": "2018-12-31",
-    "product": "Alabama",
-    "Collegegrad %": 25.5,
+    "state": "Alabama",
+    "grad%": 25.5,
     "scores": 1143,
     "spending": 10116.28614,
     "revenueper": 15
   },
   {
     "date": "2019-12-31",
-    "product": "California",
+    "state": "California",
     "grad%": 33.9,
     "scores": 1065,
     "spending": 14053.17631,
@@ -17,200 +17,200 @@ const data=[
   },
   {
     "date": "2020-12-31",
-    "product": "Florida",
-    "Collegegrad %": 29.9,
+    "state": "Florida",
+    "grad%": 29.9,
     "scores": 999,
     "spending": 9936.722694,
     "revenueper": 19
   },
   {
     "date": "2018-12-31",
-    "product": "Maryland",
-    "Collegegrad %": 40.2,
+    "state": "Maryland",
+    "grad%": 40.2,
     "scores": 1058,
     "spending": 	14643.81636,
     "revenueper": 21
   },
   {
     "date": "2019-12-31",
-    "product": "Massachusetts",
-    "Collegegrad %": 43.7,
+    "state": "Massachusetts",
+    "grad%": 43.7,
     "scores": 1120,
     "spending": 18268.60485,
     "revenueper": 3
   },
   {
     "date": "2020-12-31",
-    "product": "Michigan",
-    "Collegegrad %": 29.1,
+    "state": "Michigan",
+    "grad%": 29.1,
     "scores": 1003,
     "spending": 	13071.80858,
     "revenueper": 16
   },
   {
     "date": "2018-12-31",
-    "product": "Minnesota",
-    "Collegegrad %": 	36.1,
+    "state": "Minnesota",
+    "grad%": 	36.1,
     "scores": 1284,
     "spending": 13603.18324,
     "revenueper": 16
   },
   {
     "date": "2019-12-31",
-    "product": "New Jersey",
-    "Collegegrad %": 39.7,
+    "state": "New Jersey",
+    "grad%": 39.7,
     "scores": 1090,
     "spending": 	20629.6443,
     "revenueper": 25
   },
   {
     "date": "2020-12-31",
-    "product": "New York",
-    "Collegegrad %": 36.6,
+    "state": "New York",
+    "grad%": 36.6,
     "scores": 1064,
     "spending": 25520.36024,
     "revenueper": 4
   },
   {
     "date": "2018-12-31",
-    "product": "Ohio",
-    "Collegegrad %": 28.3,
+    "state": "Ohio",
+    "grad%": 28.3,
     "scores": 	1097,
     "spending": 13804.54123,
     "revenueper": 12
   },
   {
     "date": "2019-12-31",
-    "product": "Oklahoma",
-    "Collegegrad %": 25.5,
+    "state": "Oklahoma",
+    "grad%": 25.5,
     "scores": 963,
     "spending": 9512.041651,
     "revenueper": 13
   },
   {
     "date": "2020-12-31",
-    "product": "Pennsylvania",
-    "Collegegrad %": 	31.4,
+    "state": "Pennsylvania",
+    "grad%": 	31.4,
     "scores": 1082,
     "spending": 17141.64616,
     "revenueper": 3
   },
   {
     "date": "2018-12-31",
-    "product": "Washington",
-    "Collegegrad %": 36,
+    "state": "Washington",
+    "grad%": 36,
     "scores": 1074,
     "spending": 	14555.71182,
     "revenueper": 22
   },
   {
     "date": "2019-12-31",
-    "product": "West Virginia",
-    "Collegegrad %": 	20.6,
+    "state": "West Virginia",
+    "grad%": 	20.6,
     "scores": 	943,
     "spending": 10233.78315,
     "revenueper": 3
   },
 //   {
 //     "date": "2020-12-31",
-//     "product": "Product E",
-//     "Collegegrad %": 2485,
+//     "state": "state E",
+//     "grad%": 2485,
 //     "scores": 2957.15,
 //     "spending": 472.15,
 //     "revenueper": 19
 //   },
 //   {
 //     "date": "2018-12-31",
-//     "product": "Product F",
-//     "Collegegrad %": 1990,
+//     "state": "state F",
+//     "grad%": 1990,
 //     "scores": 2407.9,
 //     "spending": 417.9,
 //     "revenueper": 21
 //   },
 //   {
 //     "date": "2019-12-31",
-//     "product": "Product F",
-//     "Collegegrad %": 8034,
+//     "state": "state F",
+//     "grad%": 8034,
 //     "scores": 9399.78,
 //     "spending": 1365.78,
 //     "revenueper": 17
 //   },
 //   {
 //     "date": "2020-12-31",
-//     "product": "Product F",
-//     "Collegegrad %": 13254,
+//     "state": "state F",
+//     "grad%": 13254,
 //     "scores": 14314.32,
 //     "spending": 1060.32,
 //     "revenueper": 8
 //   },
 //   {
 //     "date": "2018-12-31",
-//     "product": "Product G",
-//     "Collegegrad %": 18545,
+//     "state": "state G",
+//     "grad%": 18545,
 //     "scores": 21141.3,
 //     "spending": 2596.3,
 //     "revenueper": 14
 //   },
 //   {
 //     "date": "2019-12-31",
-//     "product": "Product G",
-//     "Collegegrad %": 6168,
+//     "state": "state G",
+//     "grad%": 6168,
 //     "scores": 7524.96,
 //     "spending": 1356.96,
 //     "revenueper": 22
 //   },
 //   {
 //     "date": "2020-12-31",
-//     "product": "Product G",
-//     "Collegegrad %": 12382,
+//     "state": "state G",
+//     "grad%": 12382,
 //     "scores": 14610.76,
 //     "spending": 2228.76,
 //     "revenueper": 18
 //   },
 //   {
 //     "date": "2018-12-31",
-//     "product": "Product H",
-//     "Collegegrad %": 12498,
+//     "state": "state H",
+//     "grad%": 12498,
 //     "scores": 14622.66,
 //     "spending": 2124.66,
 //     "revenueper": 17
 //   },
 //   {
 //     "date": "2019-12-31",
-//     "product": "Product H",
-//     "Collegegrad %": 8214,
+//     "state": "state H",
+//     "grad%": 8214,
 //     "scores": 8953.26,
 //     "spending": 739.26,
 //     "revenueper": 9
 //   },
 //   {
 //     "date": "2020-12-31",
-//     "product": "Product H",
-//     "Collegegrad %": 3361,
+//     "state": "state H",
+//     "grad%": 3361,
 //     "scores": 3865.15,
 //     "spending": 504.15,
 //     "revenueper": 15
 //   },
 //   {
 //     "date": "2018-12-31",
-//     "product": "Product I",
-//     "Collegegrad %": 16152,
+//     "state": "state I",
+//     "grad%": 16152,
 //     "scores": 18413.28,
 //     "spending": 2261.28,
 //     "revenueper": 14
 //   },
 //   {
 //     "date": "2019-12-31",
-//     "product": "Product I",
-//     "Collegegrad %": 2303,
+//     "state": "state I",
+//     "grad%": 2303,
 //     "scores": 2349.06,
 //     "spending": 46.06,
 //     "revenueper": 2
 //   },
 //   {
 //     "date": "2020-12-31",
-//     "product": "Product I",
-//     "Collegegrad %": 14806,
+//     "state": "state I",
+//     "grad%": 14806,
 //     "scores": 18063.32,
 //     "spending": 3257.32,
 //     "revenueper": 22
